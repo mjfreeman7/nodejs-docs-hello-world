b02dc16244b8e6f4840957983f6a4a5a94c8a5b9
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2019-08-23 16:13:58 +0100

    updated app

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const http = require('http');
+// Simple web server that says hello and local IP address of the server
+var express = require('express');
+var app = express();
+var os = require('os');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! New Test - Again");
+// Finds IPv4 interface IP for specificed interface name
+// the second private interface name for the VM is 'eth0', the first used as NAT interface
+const interfaces = os.networkInterfaces();
+const interface_name = 'eth0'; 
+const interface = interfaces[interface_name];
+const interface_ip = interface ? interface.find(int => int.family == 'IPv4').address : 'Unknown IP';
+
+// Server port
+const port = 80;
+
+// Routes
+app.get('/', async (req, res) => {    
+    // Custom header for test cases
+    res.header('x-served-from', interface_ip);
+
+    // Response with server IP 
+    res.send(`Hello AL! I am ${interface_ip}`);
+
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+// Start the app
+app.listen(port);
